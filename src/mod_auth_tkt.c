ea4925b401171a69f52042d07238fb246b939fe4
Koen Wilde <koen@chillheid.nl>
2023-06-01 16:22:05 +0200

    Bump version

--- src/mod_auth_tkt.c
+++ src/mod_auth_tkt.c
@@ -44,7 +44,7 @@
 #define FORCE_REFRESH 1
 #define CHECK_REFRESH 0
 
-#define TKT_AUTH_VERSION "2.3.99b1"
+#define TKT_AUTH_VERSION "2.3.99b2"
 
 /* ----------------------------------------------------------------------- */
 /* Per-directory configuration */
